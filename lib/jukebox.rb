98af3ea557e6e20af13a464da1f8d55f1f82ff4f
Brad Newman <newmanbradm@gmail.com>
2018-05-20 14:32:42 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -12,5 +12,7 @@ songs = [
 
 def help
   puts "I accept the following commands:"
-  puts "- help: displays this help message"
-  puts "- list"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : "
