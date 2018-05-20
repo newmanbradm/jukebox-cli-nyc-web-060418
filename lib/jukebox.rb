6db8ec9e7bf692638128ba49fb52f0805763c81c
Brad Newman <newmanbradm@gmail.com>
2018-05-20 15:04:47 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -31,7 +31,7 @@ def play(songs)
   input = gets.chomp
   if songs.include?(input)
     puts "Playing #{input}"
-  elsif (1..9)to_a.include?(input.to_i)
+  elsif (1..9).to_a.include?(input.to_i)
     puts "Playing #{songs[input.to_i - 1]}"
   else
     puts "Invalid input, please try again."
