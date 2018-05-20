68b4bcdc71628d9a2d73a4dd157d8fe781696988
Brad Newman <newmanbradm@gmail.com>
2018-05-20 14:32:52 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -15,4 +15,6 @@ def help
   puts "- help : displays this help message"
   puts "- list : displays a list of songs you can play"
   puts "- play : lets you choose a song to play"
-  puts "- exit : "
+  puts "- exit : exits this program"
+end
+
